79936f2f1e80ac48ba9b96d4315c7253df7785dc
somesh <somesh47.karmakar@gmail.com>
2023-03-21 20:17:53 +0530

    fitnesso navbar

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-const toggle_btn = document.getElementById('toggle')
-const nav = document.getElementById('nav-right')
+const toggle_btn = document.getElementById('ham')
+const nav = document.getElementById('nav')
+const hamhh = document.querySelectorAll('.ham-hh')
+
 
 toggle_btn.addEventListener('click',()=>{
-    if(nav.classList.contains('flex')){
-        
-        nav.classList.remove('flex')
-        nav.classList.toggle('hidden')
+    if(nav.classList.contains('h-20')){
+        for(let i of hamhh){
+            i.classList.add('cross')
+        }
+        nav.classList.remove('h-20')
+        nav.classList.toggle('h-[320px]')
     }else{
-        
-        nav.classList.remove('hidden')
-        nav.classList.toggle('flex')
+        for(let i of hamhh){
+            i.classList.remove('cross')
+        }
+        nav.classList.remove('h-[320px]')
+        nav.classList.toggle('h-20')
     }
     
 })
